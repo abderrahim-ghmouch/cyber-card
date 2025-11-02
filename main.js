7f9d992cfdb9741fbc6e7410dac744598df7c5bc
abderrahim.ghmouch <abderrahimghmouch47@gmail.com>
2025-11-02 10:28:14 +0100

    first-one

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-const test = {};
 
-function User() {
-  this.name;
-  this.age;
 
-  this.setName = function (name) {
-    this.name = name;
-  };
 
-  this.setAge = function (age) {
-    this.age = age;
-  };
-
-  this.toString = function () {
-    return `this is ${this.name} his age is ${this.age} year old`;
-  };
-}
-
-class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  
-  toString = function () {
-    return `this is ${this.name} his age is ${this.age} year old`;
-  };
-}
-
-const mohammed = new User();
-mohammed.setName(`mohammed`);
-mohammed.setAge(20);
-console.log(mohammed.toString());
-
-const abdrahim = new User(`abdrahim`,19);
-console.log(mohammed.toString());
